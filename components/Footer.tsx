709aee982f493bdbcdcd060a40f41ebe902504cd
chiragraut7 <chiragraut7@gmail.com>
2026-02-25 13:24:55 +0530

    Add components/Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className='max-w-7xl mx-auto py-4 flex justify-between items-center'>
+      <p className='text-lg'>2024 Luxury Hotel. All rights reserved.</p>
+      <ul className='flex items-center space-x-4'>
+        <li><a href='#' className='text-lg'>Terms of Service</a></li>
+        <li><a href='#' className='text-lg'>Privacy Policy</a></li>
+      </ul>
+    </footer>
+  );
+}
